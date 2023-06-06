bd746e67b96016343e965bdf33331ec029d3c980
Avivapradasyah <Pradasyahaviva>
2023-06-07 06:37:18 +0700

    first commit

---
+++ pertemuan8/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+
+</script>
+
+
+
+<template>
+  <TugasPW/>
+  
+
+</template>
+
+<script>
+import TugasPW from './components/TugasPW.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TugasPW,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+</style>
